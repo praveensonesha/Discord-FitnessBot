15fca6d0834a61cd42ee1df8f4d5b9dc6e65757d
psonesha <praveensonesha2003@gmail.com>
2024-08-29 12:58:06 +0530

    db cofig updated

--- dbConfig.js
+++ dbConfig.js
@@ -10,10 +10,10 @@ module.exports = {
     queueLimit: 0
   },
   cube_club: {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'cube_club',
+    host: process.env.DB_HOST_1,
+    user: process.env.DB_USERNAME_1,
+    password: process.env.DB_PASS_1,
+    database: process.env.DB_NAME_1,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
